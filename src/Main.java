48bd73aaccb004f03f21b362c06f4b760fdcc364
Kozyaruk Vadim <kozyaruk@mail.ru>
2022-09-02 22:21:46 +0300

    add croppAverage

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         SalesManager salesManager = new SalesManager(new int[]{5, 77, 31, 22});
         System.out.println("Max sale: " + salesManager.max());
+        System.out.println("croppAverage: " + salesManager.croppAverage());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,44 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = -1;
+        if (sales.length>0) {
+            min = sales[0];
+        }
+
+        if (sales.length > 0) {
+            for (int sale : sales) {
+                if (sale < min) {
+                    min = sale;
+                }
+            }
+        }
+        return min;
+    }
+
+    public int croppAverage() {
+        int result = -1;
+        int min = min();
+        int max = max();
+
+        int summ = 0;
+
+        for (int sale : sales) {
+            if (sale == max) {
+                continue;
+            }
+            if (sale == min) {
+                continue;
+            }
+            summ += sale;
+
+        }
+
+        if (summ > 0 && sales.length > 2) {
+            result = summ / (sales.length - 2);
+        }
+        return result;
+    }
 }
